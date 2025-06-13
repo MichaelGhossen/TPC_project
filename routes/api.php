a5e09dd2e9a46c982e542dfc2ca4608c15c45f17
Michael Ghossen <michaelghossen@gmail.com>
2025-06-13 12:33:57 +0300

    Remove Google OAuth Client Secret

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+//for admin
 Route::middleware('auth:sanctum')->get('/showAllUsers', [UserController::class, 'showAllUsers']);
 Route::middleware('auth:sanctum')->put('/admin/users/{id}', [UserController::class, 'updateUserById']);
 
@@ -24,28 +25,3 @@ Route::middleware('auth:sanctum')->put('/admin/users/{id}', [UserController::cla
     Route::put('/user/update/{id}', [UserController::class, 'update']);
     Route::delete('/user/delete/{id}', [UserController::class, 'destroy']);
 
-    Route::middleware('auth:sanctum')->group(function () {
-        Route::get('/names', [NameController::class, 'index']);
-        Route::get('/name/{id}', [NameController::class, 'show']);
-
-        //  only for admin:
-        Route::post('/name/store', [NameController::class, 'store']);
-        Route::put('/name/{id}', [NameController::class, 'update']);
-        Route::delete('/name/{id}', [NameController::class, 'destroy']);
-    });
-
-    Route::middleware('auth:sanctum')->group(function () {
-        Route::get('/raw-materials', [RawMaterialController::class, 'index']);
-        Route::post('/raw-materials', [RawMaterialController::class, 'store']);
-        Route::get('/raw-materials/{id}', [RawMaterialController::class, 'show']);
-        Route::put('/raw-materials/{id}', [RawMaterialController::class, 'update']);
-        Route::delete('/raw-materials/{id}', [RawMaterialController::class, 'destroy']);
-    });
-
-    Route::middleware('auth:sanctum')->group(function () {
-        Route::get('/products', [ProductController::class, 'index']);
-        Route::post('/products', [ProductController::class, 'store']);
-        Route::get('/products/{id}', [ProductController::class, 'show']);
-        Route::put('/products/{id}', [ProductController::class, 'update']);
-        Route::delete('/products/{id}', [ProductController::class, 'destroy']);
-    });
